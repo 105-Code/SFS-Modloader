64ba5a9d56726ea6d6291ee23fb3e88ef79e8968
dani0105 <danielrojas0105@gmail.com>
2022-03-31 19:52:13 -0600

    Add KeybindingHelper

---
+++ ModLoader/Helpers/KeybindingHelper.cs
@@ -0,0 +1,35 @@
+﻿using SFS.Builds;
+using SFS.Input;
+using SFS.World;
+using System;
+
+namespace ModLoader.Helpers
+{
+    /// <summary>
+    /// Use this class if you need add key events.
+    /// </summary>
+    static class KeybindingHelper
+    {
+
+        /// <summary>
+        /// Add onKeyDown on world and map
+        /// </summary>
+        /// <param name="key">key to trigger the event</param>
+        /// <param name="action"> is executed when the event occurs</param>
+        public static void AddOnKeyDownWorld(I_Key key, Action action)
+        {
+            GameManager.main.world_Input.keysNode.AddOnKeyDown(key, action);
+            GameManager.main.map_Input.keysNode.AddOnKeyDown(key, action);
+        }
+
+        /// <summary>
+        /// Add onKeyDown on builder
+        /// </summary>
+        /// <param name="key">key to trigger the event</param>
+        /// <param name="action"> is executed when the event occurs</param>
+        public static void AddOnKeyDownBuilder(I_Key key, Action action)
+        {
+            BuildManager.main.build_Input.keysNode.AddOnKeyDown(key, action);
+        }
+    }
+}
