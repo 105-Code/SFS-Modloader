d0ccf162ae3ce86b67b2f97f85d47d50e32191e5
dani0105 <danielrojas0105@gmail.com>
2022-04-17 10:37:06 -0600

    Add unitTest project

---
+++ ModLoaderTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ModLoaderTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModLoaderTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5b6e9d9-31aa-48c4-976d-7f8d45adb982")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ModLoaderTesting/TestLoader.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModLoader;
+
+namespace ModLoaderTesting
+{
+    [TestClass]
+    public class TestLoader
+    {
+        private PrivateObject _loader;
+
+        [TestInitialize]
+        public void Init()
+        {
+            Loader loader = new Loader();
+            this._loader = new PrivateObject(loader);
+        }
+
+        [TestMethod]
+        public void DifferentMajorVersion_verifyVersion_ReturnFalse()
+        {
+            // Arrange
+            string currentVersion = "v2.1.1";
+            string targetVersion = "v1.x.x";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TargetCompatibleWithAllMinorVersions_verifyVersion_ReturnTrue()
+        {
+            // Arrange
+            string currentVersion = "v1.9.1";
+            string targetVersion = "v1.x.x";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TargetHasX_verifyVersion_ReturnTrue()
+        {
+            // Arrange
+            string currentVersion = "v1.1.1";
+            string targetVersion = "v1.1.x";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TargetLessThatCurrent_verifyVersion_ReturnTrue()
+        {
+            // Arrange
+            string currentVersion = "v1.3.0";
+            string targetVersion = "v1.2.2";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool) this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void TargetGreaterThatCurrent_verifyVersion_ReturnFalse()
+        {
+            // Arrange
+            string currentVersion = "v1.1.1";
+            string targetVersion = "v1.1.2";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TargetEqualCurrent_verifyVersion_ReturnTrue()
+        {
+            // Arrange
+            string currentVersion = "v1.1.1";
+            string targetVersion = "v1.1.1";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CurrentInvalidFormat_verifyVersion_ReturnFalse()
+        {
+            // Arrange
+            string currentVersion = "v1.1.1 beta";
+            string targetVersion = "v1.1.1";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TargetInvalidFormat_verifyVersion_ReturnFalse()
+        {
+            // Arrange
+            string currentVersion = "v1.1.1";
+            string targetVersion = "v1.1.1 beta";
+            object[] toTest = new object[] { targetVersion, currentVersion, true };
+
+            // Act
+            bool result = (bool)this._loader.Invoke("verifyVersion", toTest);
+
+            // Assert 
+            Assert.IsFalse(result);
+        }
+
+        
+    }
+}
